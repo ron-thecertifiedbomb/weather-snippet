fa2f4b267a3f8165fd9235838ca182e6cd57bb97
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-09-21 11:44:32 +0800

    changed ui

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -4,43 +4,57 @@ import React from "react";
 const Weather = ({ data }) => {
   console.log(data);
   return (
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[80vh] m-auto p-4 text-gray-300 z-10">
+    <div className="flex flex-col h-full m-auto p-4 pt-10 pb-5 text-gray-300 z-10">
       {/* Top */}
-      <div className="relative flex justify-between pt-12">
-        <div className="flex flex-col items-center">
+         <div className="flex flex-col  items-center">
           <Image
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="/"
+            width="130"
+            height="130"
+          /> 
+          {/* <p className="text-2xl">{data.weather[0].main}</p> */}
+        </div> 
+        <div className="flex flex-col justify-center items-center"> 
+        <p className="text-[18px] text-center mb-4">{data.name}</p>
+        {/* <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt="/"
             width="100"
             height="100"
-          />
-          <p className="text-2xl">{data.weather[0].main}</p>
+          /> */}
+       
+        <p className="text-6xl lg:text-7xl lg:pt-0">{data.main.temp.toFixed(0)}&#176;</p>
+        
+    
+       
+ 
         </div>
-        <p className="text-9xl">{data.main.temp.toFixed(0)}&#176;</p>
-      </div>
-      {/* Bottom */}
+   
 
-      <div className="bg-black/50 relative p-8 rounded-md">
-        <p className="text-2xl text-center pb-6">Weather in {data.name}</p>
-        <div className="flex justify-between text-center">
+       <div className="p-5 rounded-md">
+      
+        <div className="flex gap-5 text-center justify-center">
           <div>
-            <p className="font-bold text-2xl">
+            <p className=" font-normal text-xl">
               {data.main.feels_like.toFixed(0)}&#176;
             </p>
-            <p className="text-xl">Feels Like</p>
+            <p className="text-xs">Feels Like</p>
           </div>
           <div>
-            <p className="font-bold text-2xl">{data.main.humidity}%</p>
-            <p className="text-xl">Humidity</p>
+            <p className="font-normal text-xl">{data.main.humidity}%</p>
+            <p className="text-xs">Humidity</p>
           </div>
           <div>
-            <p className="font-bold text-2xl">
-              {data.wind.speed.toFixed(0)} MPH
+            <p className="font-normal text-xl">
+              {data.wind.speed.toFixed(0)} 
             </p>
-            <p className="text-xl">Winds</p>
+            <p className=" text-xs">Winds</p>
           </div>
         </div>
       </div>
+
+      
     </div>
   );
 };
